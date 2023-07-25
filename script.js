039e92f0562c6431eb258184945c2fa26a590e91
Dara Ferreira <dara-ingrid@hotmail.com>
2023-07-25 14:35:09 -0300

    Alteração de imagem via js ao clicar no botao

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 function toggleMode() {
   const html = document.documentElement
-  /*
-  if (html.classList.contains('light')){
-    html.classList.remove('light')
-  } else {
-    html.classList.add('light')
-  }
-  */
-
   html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    //se tiver a imagem light mode, adicionar a imagem light
+    img.setAttribute('src', './assets/avatar-light.png')
+    img.setAttribute(
+      "alt",
+      "Foto do Mayk Brito sorrindo, usando óculos escuro e camisa preta, barba e fundo azul e rosa"
+    )
+  } else {
+    //se tiver sem light mode, manter a imagem normal;
+    img.setAttribute('src', "./assets/avatar.png")
+  }
+
+  //substituir a imagem
 }
